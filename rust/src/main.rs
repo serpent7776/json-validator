73e666924593195b7ea9bd4a62c2fde614dee022
Serpent7776 <Serpent7776@gmail.com>
2023-07-05 17:32:05 +0200

    Wrap stdin in BufReader

--- rust/src/main.rs
+++ rust/src/main.rs
@@ -3,7 +3,7 @@ use std::io::Read;
 use json_validator::validate_bytes;
 
 fn main() {
-    match validate_bytes(std::io::stdin().bytes()) {
+    match validate_bytes(std::io::BufReader::new(std::io::stdin()).bytes()) {
         Ok(()) => println!("Ok"),
         Err((err, pos)) => println!("Error {} at {}", err, pos),
     }
